767bba7d4c32cf7c9a599f98d1abfadb0210e180
8adiq <abdul.latif708@yahoo.com>
2024-11-16 09:09:06 -0500

    changed abstractuser to abstractbaseuser

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,9 +1,51 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser,Group,Permission
+from django.contrib.auth.models import BaseUserManager,PermissionsMixin,AbstractBaseUser
 from django.conf import settings
 from django.utils import timezone
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+
+    def create_user(self,name,email,role,password,manager=None,agent=None):
+        if not email:
+            raise ValueError('Email has been provided')
+        if not name:
+            raise ValueError('Name has to be provided')
+        if not password:
+            raise ValueError('User must have a password')
+        if not role:
+            raise ValueError('Role has to be provided')
+        if not manager or not agent:
+            raise ValueError('User has to be connected to a manager or an agent')
+
+        user = self.model (
+            email = self.normalize_email(email),
+            name=name,
+            manager=manager,
+            role=role,
+            agent=agent 
+        )
+        user.set_password(password)
+        user.save()
+        return user 
+    
+    def create_superuser(self,name,email,password):
+        if not email:
+            raise ValueError('Email has been provided')
+        if not name:
+            raise ValueError('Name has to be provided')
+        if not password:
+            raise ValueError('User must have a password')
+        
+        user = self.model(
+            email = self.normalize_email(email),
+            name=name
+        )
+        user.set_password(password)
+        user.is_staff, user.is_superuser, user.is_admin = True, True, True
+        user.save()
+        return user
+    
+class User(AbstractBaseUser,PermissionsMixin):
     MANAGER = 'manager'
     AGENT = 'agent'
     CLIENT = 'client'
@@ -20,8 +62,14 @@ class User(AbstractUser):
     manager= models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.SET_NULL,related_name='agents',null=True,limit_choices_to={'role':MANAGER},help_text='manager oversees this client or agent')
     agent = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.SET_NULL,related_name='clients',null=True,limit_choices_to={'role':AGENT}, help_text='agent oversees this client')
     job = models.TextField(blank=True,null=True)
+    is_staff = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(default=timezone.now)
 
+    objects = UserManager()
+    
     REQUIRED_FIELDS = ['name']
     USERNAME_FIELD = 'email'
 
